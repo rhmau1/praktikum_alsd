b9bd6b030108f75d986ed03a3c771e0cba388e26
rahma <fijriatirahmaturrizqi@gmail.com>
2025-02-21 13:12:50 +0700

    fix: update output MahasiswaMain

--- pertemuan2/MahasiswaMain10.java
+++ pertemuan2/MahasiswaMain10.java
@@ -9,15 +9,19 @@ public class MahasiswaMain10 {
         mhs1.ipk = 3.9;
 
         mhs1.tampilkanInformasi();
+        System.out.println("==========");
         mhs1.ubahKelas("TI 1C");
         mhs1.updateIpk(4.0);
         mhs1.tampilkanInformasi();
 
+        System.out.println("==========");
         Mahasiswa10 mhs2 = new Mahasiswa10("Nina", "123", 3.5, "TI 1D");
         mhs2.updateIpk(3.30);
         mhs2.tampilkanInformasi();
 
+        System.out.println("==========");
         Mahasiswa10 mhsFijriati = new Mahasiswa10("Fijriati Rahmatur Rizqi", "244107020069", 4, "TI 1H");
         mhsFijriati.tampilkanInformasi();
+        System.out.printf("Hasil nilai kinerja Fijriati adalah: %s\n", mhsFijriati.nilaiKinerja());
     }
 }
